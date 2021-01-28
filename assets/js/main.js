d0ac1f65fba075e5944d8aa2b5c16cf3e08674d5
ZenVega <ufogreeks@gmail.com>
2021-01-28 19:37:57 +0100

    language pack

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,13 @@ const ruBtn = document.querySelector('#ru-btn')
 const deBtn = document.querySelector('#de-btn')
 const langBtns = [engBtn, ruBtn, deBtn]
 
+const aboutBtn = document.querySelector('#about')
+
 const txtColor1 = '#FFFDFD'
 const txtColor2 = '#554C49'
 
 const aboutTxt = document.querySelector('#about-text')
-
-
-var langPack
-fetch('..data/texts.json')
-.then(response => {
-   return response.json();
-})
-.then(data => langPack = data);
-console.log(langPack)
+console.log(engBtn.innerHTML)
 
 const switchLanguage = (target) => {
 
@@ -29,16 +23,22 @@ const switchLanguage = (target) => {
   switch (id) {
     case "eng-btn":
       engBtn.style.color = txtColor1
+      replaceHtml("eng")
       break;
     case "de-btn":
       deBtn.style.color = txtColor1
+      replaceHtml("de")
       break;
     case "ru-btn":
       ruBtn.style.color = txtColor1
+      replaceHtml("ru")
       break;
 
   }
 }
 
+const replaceHtml = (lang) => {
+  aboutTxt.innerHTML = langPack[lang].about
+}
 
 langBtns.forEach(btn => btn.addEventListener('click', e => switchLanguage(e.target)))
