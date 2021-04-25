f055eecf244767519b57831607d1018fd59b8b2c
ZenVega <ufogreeks@gmail.com>
2021-04-25 14:17:28 +0200

    image color responsive

--- assets/js/startup.js
+++ assets/js/startup.js
@@ -4,22 +4,40 @@ const imageWrapper = document.querySelector(".image-wrapper")
 const imgAmount = 13
 const imgIdArray = [...Array(imgAmount).keys()]
 
-const shuffledImages = imgIdArray
+let shuffledImages = imgIdArray
   .map((a) => ({sort: Math.random(), value: a}))
   .sort((a, b) => a.sort - b.sort)
   .map((a) => a.value)
 
 
-const imgArray = shuffledImages.map(id => {
-  let img = document.createElement('img')
-  img.src = `assets/images/img${id}.jpg`
-  return img
-})
+  
+const changeStyle = id => {
+  
+  if(imgPack[id].frontPage){
+    document.documentElement.style.setProperty('--bg-color-1', imgPack[id].bg2);
+    document.documentElement.style.setProperty('--bg-color-2', imgPack[id].bg1);
+    document.documentElement.style.setProperty('--txt-color-2', imgPack[id].txt1);
 
-previewImage.src = imgArray[0].src
 
-for(i = 1; i <= 9; i++){
-  console.log(imgArray[i])
-  imageWrapper.appendChild(imgArray[i])
+    let imgArray = shuffledImages.map(id => {
+      let img = document.createElement('img')
+      img.src = `assets/images/img${id}.jpg`
+      return img
+    })
+    
+    previewImage.src = imgArray[0].src
+    
+    for(i = 1; i <= 9; i++){
+      imageWrapper.appendChild(imgArray[i])
+    }
+
+  } else {
+    shuffledImages.push(shuffledImages.shift())
+    changeStyle(shuffledImages[0])
+  }
 }
-console.log(imgArray[0].src)
+  
+changeStyle(shuffledImages[0])
+
+
+
